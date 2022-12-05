61c9bc45832262c8ccbd12d179d957d1c9e791a6
DominikWawak <dominikwawak@gmail.com>
2022-12-05 11:45:02 +0000

    Default unknown face

--- server.py
+++ server.py
@@ -1,25 +1,30 @@
 import eventlet
 from livevideo import app
+
 import socketio
 from waitress import serve
-
+import socket
+ 
+hostname=socket.gethostname()   
+IPAddr=socket.gethostbyname(hostname)  
 sio = socketio.Server()
 appServer = socketio.WSGIApp(sio, app)
 
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
+# @sio.event
+# def connect(sid, environ):
+#     print('connect ', sid)
 
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
+# @sio.event
+# def my_message(sid, data):
+#     print('message ', data)
 
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+# @sio.event
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 if __name__ == '__main__':
     # eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), appServer)
-    print("Server started connect with http://127.0.0.1:8080 or http://localip:8080 - 6 clients max")
-    serve(app, host='0.0.0.0', port=8080, url_scheme='RTMP', threads=6)
+    print("Server started connect with http://127.0.0.1:8080 or http://"+IPAddr+":8080 - 6 clients max")
+    serve(appServer, host='0.0.0.0', port=8080, url_scheme='http', threads=6)
+    # threads=6,expose_tracebacks=True,log_untrusted_proxy_headers=True
 
